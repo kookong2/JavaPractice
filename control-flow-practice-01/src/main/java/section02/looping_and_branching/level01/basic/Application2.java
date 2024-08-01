b144f4b98c73c7b8e07770f19c9d2a53c748e1dd
kookong2 <hy010309@naver.com>
2024-08-01 17:46:21 +0900

    [FEAT] 입력 후 합계 문제 완료

---
+++ control-flow-practice-01/src/main/java/section02/looping_and_branching/level01/basic/Application2.java
@@ -0,0 +1,28 @@
+package section02.looping_and_branching.level01.basic;
+
+import java.util.Scanner;
+
+public class Application2 {
+    public static void main(String[] args) {
+        /* 정수 한 개를 입력 받고, 1부터 입력 받은 정수까지의 합을 계산해서 출력하세요
+         *
+         * -- 입력 예시 --
+         * 정수를 입력하세요 : 5
+         *
+         * -- 출력 예시 --
+         * 1부터 5까지의 합 : 15
+         *
+         * */
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("정수를 입력하세요 : ");
+        int x = in.nextInt();
+
+        int result = 0;
+        for(int i = 1; i <= x; i++) {
+            result += i;
+        }
+
+        System.out.println("1부터 " + x +"까지의 합 : " + result);
+    }
+}
