0a0342e57a588ac2d0aa3a0e3d5a95767b09722a
kookong2 <hy010309@naver.com>
2024-09-05 20:00:30 +0900

    [FEAT] Product List 전체 조회 기능

--- chap99-practice/src/main/java/com/management/product/model/service/ProductService.java
+++ chap99-practice/src/main/java/com/management/product/model/service/ProductService.java
@@ -1,10 +1,14 @@
 package com.management.product.model.service;
 
 import com.common.SearchCondition;
+import com.management.product.model.dao.ProductDAO;
 import com.management.product.model.dto.ProductDTO;
+import org.apache.ibatis.session.SqlSession;
 
 import java.util.List;
 import java.util.Map;
+
+import static com.common.Template.getSqlSession;
 
 public class ProductService {
 
@@ -16,15 +20,29 @@ public class ProductService {
 
         // 2. 전체 제품 목록을 조회하는 로직을 작성하세요.
         // 　　아래 작성된 return null은 과제 툴 오류를 제거하고자 임의 작성하였으니 지우고 로직을 작성하세요.
-        return null;
+        SqlSession sqlSession = getSqlSession();
+
+        ProductDAO productDAO = sqlSession.getMapper(ProductDAO.class);
+
+        List<ProductDTO> productList = productDAO.selectAllProductList();
+
+        sqlSession.close();
+
+        return productList;
 
     }
 
     public List<ProductDTO> selectProductByCondition(SearchCondition searchCondition) {
 
-        // 3. 조건에 따른 제품 목록을 조회하는 로직을 작성하세요.
-        // 　　아래 작성된 return null은 과제 툴 오류를 제거하고자 임의 작성하였으니 지우고 로직을 작성하세요.
-        return null;
+        SqlSession sqlSession = getSqlSession();
+
+        ProductDAO productDAO = sqlSession.getMapper(ProductDAO.class);
+
+        List<ProductDTO> productList = productDAO.selectProductByCondition(searchCondition);
+
+        sqlSession.close();
+
+        return productList;
 
     }
 
