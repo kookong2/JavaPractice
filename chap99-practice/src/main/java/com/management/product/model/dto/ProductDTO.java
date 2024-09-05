5f8ded7697aa2d7d678d0288f7e874be9447789d
kookong2 <hy010309@naver.com>
2024-09-05 20:00:00 +0900

    [FEAT] Product List 전체 조회 기능

--- chap99-practice/src/main/java/com/management/product/model/dto/ProductDTO.java
+++ chap99-practice/src/main/java/com/management/product/model/dto/ProductDTO.java
@@ -1,8 +1,115 @@
 package com.management.product.model.dto;
 
 public class ProductDTO {
+    private String productCode;
+    private String productName;
+    private String originCost;
+    private String  releaseDate;
+    private String  discountRate;
+    private String  salesQuantity;
+    private String stockQuantity;
+    private String categoryCode;
+    private String productionStatus;
 
-    // * 주석을 지우고 제품 정보를 담는 객체를 만들 java class를 작성하세요.
-    // * 모든 필드는 String 타입으로 작성하시고, 'PRODUCT_INFO' 테이블과 매핑 가능해야 합니다.
+    public ProductDTO(){}
 
+    public ProductDTO(String productCode, String productName, String originCost, String releaseDate, String discountRate, String salesQuantity, String stockQuantity, String categoryCode, String productionStatus) {
+        this.productCode = productCode;
+        this.productName = productName;
+        this.originCost = originCost;
+        this.releaseDate = releaseDate;
+        this.discountRate = discountRate;
+        this.salesQuantity = salesQuantity;
+        this.stockQuantity = stockQuantity;
+        this.categoryCode = categoryCode;
+        this.productionStatus = productionStatus;
+    }
+
+    public String getProductionStatus() {
+        return productionStatus;
+    }
+
+    public void setProductionStatus(String productionStatus) {
+        this.productionStatus = productionStatus;
+    }
+
+    public String getCategoryCode() {
+        return categoryCode;
+    }
+
+    public void setCategoryCode(String categoryCode) {
+        this.categoryCode = categoryCode;
+    }
+
+    public String getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(String stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+
+    public String getSalesQuantity() {
+        return salesQuantity;
+    }
+
+    public void setSalesQuantity(String salesQuantity) {
+        this.salesQuantity = salesQuantity;
+    }
+
+    public String getDiscountRate() {
+        return discountRate;
+    }
+
+    public void setDiscount_Rate(String discount_Rate) {
+        this.discountRate = discount_Rate;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getOriginCost() {
+        return originCost;
+    }
+
+    public void setOriginCost(String originCost) {
+        this.originCost = originCost;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductCode() {
+        return productCode;
+    }
+
+    public void setProductCode(String productCode) {
+        this.productCode = productCode;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductDTO{" +
+                "productCode='" + productCode + '\'' +
+                ", productName='" + productName + '\'' +
+                ", originCost='" + originCost + '\'' +
+                ", releaseDate='" + releaseDate + '\'' +
+                ", discountRate='" + discountRate + '\'' +
+                ", salesQuantity='" + salesQuantity + '\'' +
+                ", stockQuantity='" + stockQuantity + '\'' +
+                ", categoryCode='" + categoryCode + '\'' +
+                ", productionStatus='" + productionStatus + '\'' +
+                '}';
+    }
 }
+
