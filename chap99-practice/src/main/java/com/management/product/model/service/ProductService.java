7c394a1862ff7be43e7e8c2debc269763e29ea54
kookong2 <hy010309@naver.com>
2024-09-05 20:45:39 +0900

    [FEAT] 제품 등록 기능 구현

--- chap99-practice/src/main/java/com/management/product/model/service/ProductService.java
+++ chap99-practice/src/main/java/com/management/product/model/service/ProductService.java
@@ -48,9 +48,20 @@ public class ProductService {
 
     public boolean registNewProduct(ProductDTO product) {
 
-        // 4. 제품 정보를 등록하는 로직을 작성하세요.
-        // 　　아래 작성된 return false 과제 툴 오류를 제거하고자 임의 작성하였으니 지우고 로직을 작성하세요.
-        return false;
+        SqlSession sqlSession = getSqlSession();
+        ProductDAO productDAO = sqlSession.getMapper(ProductDAO.class);
+
+        int result = productDAO.registNewProduct(product);
+
+        if(result > 0) {
+            sqlSession.commit();
+        }else {
+            sqlSession.rollback();
+        }
+
+        sqlSession.close();
+
+        return result > 0;
 
     }
 
